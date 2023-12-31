568ec3769f2e114b34af1f48782b98383c1a09d5
makma <471842@ntti.edu.kh>
2023-12-31 12:39:49 +0700

    26. Start implementing the search screen

--- lib/consts/theme_data.dart
+++ lib/consts/theme_data.dart
@@ -27,6 +27,38 @@ class Styles {
           color: isDarkTheme ? Colors.white : Colors.black,
         ),
       ),
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        contentPadding: const EdgeInsets.all(10),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+           color: Colors.transparent,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: isDarkTheme ? Colors.white : Colors.black,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: Theme.of(context).colorScheme.error
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 1,
+            color: Theme.of(context).colorScheme.error
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
     );
   }
 }
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,12 +1,76 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_build_ecommerce/widgets/title_text.dart';
+import '../services/assets_manager.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  late TextEditingController searchTextController;
+
+  @override
+  void initState() {
+    super.initState();
+    searchTextController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    searchTextController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Search Screen'),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const TitleTextWidget(label: "Search Products"),
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: Image.asset(AssetsManager.shoppingCart),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: searchTextController,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        searchTextController.clear();
+                        FocusScope.of(context).unfocus();
+                      });
+                    },
+                    child: const Icon(Icons.clear, color: Colors.red,),
+                  ),
+                  hintText: "Search Products",
+                  hintStyle: TextStyle(
+                    color: Colors.grey.withOpacity(0.6),
+                  ),
+                ),
+                onChanged: (value) {
+                  log(searchTextController.text);
+                },
+                onSubmitted: (value) {
+                  log(searchTextController.text);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
