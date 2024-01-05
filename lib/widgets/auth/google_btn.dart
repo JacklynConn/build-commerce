33c43d8721ac86b8807897d3f7f219408c625580
makma <471842@ntti.edu.kh>
2024-01-05 10:37:49 +0700

    75. Save the user information to Firestore on Google Sign In

--- lib/widgets/auth/google_btn.dart
+++ lib/widgets/auth/google_btn.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -13,11 +14,26 @@ class GoogleButton extends StatelessWidget {
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
       final GoogleSignInAuthentication googleAuth =
           await googleUser!.authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
+      final authResults = await FirebaseAuth.instance.signInWithCredential(
+        GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        ),
       );
-      await FirebaseAuth.instance.signInWithCredential(credential);
+      if (authResults.additionalUserInfo!.isNewUser) {
+        await FirebaseFirestore.instance
+            .collection("users")
+            .doc(authResults.user!.uid)
+            .set({
+          "userId": authResults.user!.uid,
+          "userName": authResults.user!.displayName,
+          "userImage": authResults.user!.photoURL,
+          "userEmail": authResults.user!.email!.toLowerCase(),
+          "createdAt": Timestamp.now(),
+          'userWish': [],
+          'userCart': [],
+        });
+      }
       if (!context.mounted) return;
       Navigator.of(context).pushReplacementNamed(RootScreen.routeName);
     } on FirebaseException catch (error) {
